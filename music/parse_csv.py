89cf0ecb58f7d318edd1653c4271e3da54c0178e
Amit Kumar <amitkh@gmail.com>
2025-04-23 18:32:54 -0400

    this is the best thing i have made

---
+++ music/parse_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("thunderstruck.csv")
+print(df.columns)
+df['hz'] = 440 * 2**((df['note']-69)/12)
+
+# up 3 octaves
+df['hz'] *= 2
+df['hz'] *= 2
+df['hz'] *= 2
+
+with open("wf_out", 'w') as of:
+    of.write("var frequencies = [" + ','.join(df['hz'].astype(str)) + '];\n')
+    of.write("var timePoints = [" + ','.join(df['ts'].astype(str)) + '];\n')
+
+print(min(df['hz']), max(df['hz']))
